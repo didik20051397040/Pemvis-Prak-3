2096f5011dd591363a3ed514bcd0b3f12e39d955
Muhammad didik wahyudi <72702021+didik20051397040@users.noreply.github.com>
2022-03-30 23:26:41 +0700

    Add files via upload

---
+++ no 1.py
@@ -0,0 +1,17 @@
+#MUHAMMAD DIDIK WAHYUDI
+#20051397040
+#MI2020B
+
+class Stack(list):
+    push = list.append
+
+def modify_stack(symbolString):
+    stack=Stack()
+    result = []
+    for character in symbolString:
+        if character != '*':
+            stack.push(character)
+        else:
+            result.append(stack.pop())
+    return ''.join(result)
+print(modify_stack('EAS*Y*QUE***ST***IO*N***'))
---
+++ no 2.py
@@ -0,0 +1,10 @@
+#MUHAMMAD DIDIK WAHYUDI
+#20051397040
+#MI2020B
+
+desimal = int(input('Masukkan Bilangan Desimal = '))
+
+biner = bin(desimal) .replace("0b","")
+oktal = oct(desimal) .replace("0o","")
+hexa = hex(desimal) .replace("0x","")
+print(biner,oktal,hexa)
